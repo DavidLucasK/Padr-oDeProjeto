907f3f4bdc32f7454ce71835272a0faedb3d60b4
DavidLucasK <55153395+DavidLucasK@users.noreply.github.com>
2020-10-26 18:20:34 -0300

    Add files via upload

---
+++ Composite/Código/src/ContaCorrente.java
@@ -0,0 +1,19 @@
+public class ContaCorrente extends Component
+{
+	private String numConta;
+	private float saldoConta;
+	
+	public ContaCorrente(String numConta, float saldoConta) {
+		super();
+		this.numConta = numConta;
+		this.saldoConta = saldoConta;
+	}
+	
+	public String getNumConta() {
+		return numConta;
+	}
+	
+	public float getSaldo() {
+		return saldoConta;
+	}
+}
